03ff2ce14c55e430f4897dfe27ec704faa2d3b51
Abhay Kumar Sahu <abhayksahu.cs@gmail.com>
2025-03-14 23:26:25 +0530

    code phat na jaye bas

--- src/components/xr-hit-model/XrHitModel.jsx
+++ src/components/xr-hit-model/XrHitModel.jsx
@@ -1,41 +1,16 @@
 import { OrbitControls } from "@react-three/drei";
-import { useThree,useFrame } from "@react-three/fiber";
-import { Interactive, useHitTest, useXR } from "@react-three/xr";
-import { useRef, useState } from "react";
+
 import Model from "./Model";
 
 const XrHitModel = () => {
-  const reticleRef = useRef();
+
   // const modelRef=useRef();
-  const [models, setModels] = useState([]);
+ 
   
   // useFrame((state, delta) => {
   //   modelRef.current.rotation.y += delta;
   // });
 
-  const { isPresenting } = useXR();
-
-  useThree(({ camera }) => {
-    if (!isPresenting) {
-      camera.position.z = 3;
-    }
-  });
-
-  useHitTest((hitMatrix, hit) => {
-    hitMatrix.decompose(
-      reticleRef.current.position,
-      reticleRef.current.quaternion,
-      reticleRef.current.scale
-    );
-
-    reticleRef.current.rotation.set(-Math.PI / 2, 0, 0);
-  });
-
-  const placeModel = (e) => {
-    let position = e.intersection.object.position.clone();
-    let id = Date.now();
-    setModels([{ position, id }]);
-  };
 
   return (
     <>
@@ -44,28 +19,8 @@ const XrHitModel = () => {
       <directionalLight position={[5, 5, 5]} intensity={2} />
       <directionalLight position={[-5, -5, 5]} intensity={1.5} />
       <pointLight position={[0, 2, 2]} intensity={1.5} />
-      {isPresenting &&
-        models.map(({ position, id }) => {
-          return (
-            <mesh>
-                <Model scale={0.05} key={id} position={position}  />;
-            </mesh>
-          )
-        })}
-      {isPresenting && (
-        <Interactive onSelect={placeModel}>
-          <mesh ref={reticleRef} rotation-x={-Math.PI / 2}>
-            <ringGeometry args={[0.1, 0.25, 32]} />
-            <meshStandardMaterial color={"white"} />
-          </mesh>
-        </Interactive>
-      )}
-
-      {!isPresenting &&  (
-            <mesh >
-            <Model scale={0.08} />;
-          </mesh>
-          )}
+      <Model position={[0,0,-2]}/>
+     
     </>
   );
 };
--- src/components/xr-hit-model/XrHitModelContainer.jsx
+++ src/components/xr-hit-model/XrHitModelContainer.jsx
@@ -2,17 +2,16 @@ import { Canvas } from "@react-three/fiber";
 import { ARButton, XR } from "@react-three/xr";
 import XrHitModel from "./XrHitModel";
 
+
+
+
 const XrHitModelContainer = () => {
   return (
     <>
-      <ARButton
-        sessionInit={{
-          requiredFeatures: ["hit-test"],
-        }}
-      />
+      <ARButton/>
       <Canvas>
         <XR>
-          <XrHitModel />
+          <XrHitModel/>
         </XR>
       </Canvas>
     </>
